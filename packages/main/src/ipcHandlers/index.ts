94a485a8dd4f781de66e3acdf5a0bbbe5afd9472
Yves Haas <yves.haas@uni-ulm.de>
2022-07-02 19:33:14 +0200

    style: remove commented out code

--- packages/main/src/ipcHandlers/index.ts
+++ packages/main/src/ipcHandlers/index.ts
@@ -1,5 +1,2 @@
-//import { loginHandler, logoutHandler } from './loginHandlers';
-//import { createUserHandler, deleteUserHandler, retrieveUserHandler, retrieveUsersHandler, updateUserHandler, updateUserPasswordHandler } from './userHandlers';
-
 export { loginHandler, logoutHandler } from './loginHandlers';
 export { createUserHandler, deleteUserHandler, retrieveUserHandler, retrieveUsersHandler, updateUserHandler, updateUserPasswordHandler } from './userHandlers';
