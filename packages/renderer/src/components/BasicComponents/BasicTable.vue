2d165c759c5800f163956656c3acb65b84ff5ea7
ltodtuu <laurin.todt@uni-ulm.de>
2022-07-07 08:32:56 +0200

    feat: edit user

--- packages/renderer/src/components/BasicComponents/BasicTable.vue
+++ packages/renderer/src/components/BasicComponents/BasicTable.vue
@@ -46,8 +46,66 @@
         />
         <NormalButton
           :btn-text="'Edit User'"
-          @btn-click="editUser()"
+          @btn-click="showEditUserModal = !showEditUserModal"
         />
+      </div>
+    </div>
+    <div
+      v-if="showEditUserModal"
+      class="absolute inset-0 z-40 opacity-30 bg-black"
+    />
+    <div
+      v-if="showEditUserModal"
+      class="fixed overflow-x-hidden overflow-y-auto inset-0 justify-center items-center z-50 my-24"
+    >
+      <div class="relative mx-auto w-auto max-w-2xl">
+        <div class="bg-white w-full p-10 rounded shadow-xl flex flex-col">
+          <p class=" text-center font-bold text-lg">
+            Update User
+          </p>
+          <div class="mb-6">
+            <label
+              for="username"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Username</label>
+            <input
+              id="username"
+              v-model="updatedUserName"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              type="text"
+            >
+          </div>
+          <!------- first_name  ------>
+          <div class="mb-6">
+            <label
+              for="firstName"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >First Name</label>
+            <input
+              id="firstName"
+              v-model="updatedUserFirstName"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              type="text"
+            >
+          </div>
+          <!------- last_name  ------>
+          <div class="mb-6">
+            <label
+              for="lastName"
+              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >Last Name</label>
+            <input
+              id="lastName"
+              v-model="updatedUserLastName"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              type="text"
+            >
+          </div>
+          <NormalButton
+            :btn-text="'Submit Update'"
+            @click="editUser, showEditUserModal = !showEditUserModal"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -63,13 +121,23 @@
     const selectedUserIndex = ref(-1);
     const selectedUserID = ref('');
     const selectedUserUsername = ref('');
-    const editedUser = ref(true);    
+    const selectedUserFirstName = ref('');
+    const selectedUserLastName = ref(''); 
+    const showEditUserModal = ref(false);
+    const updatedUserName = ref('');
+    const updatedUserFirstName = ref('');
+    const updatedUserLastName = ref('');
 
     function selectRow(i: number, user_id: string){
             selectedUserIndex.value = i;
             selectedUserID.value = user_id;
             const selectedUser = users.value().filter((elem) => elem.id === user_id)[0];
             selectedUserUsername.value = selectedUser.username;
+            selectedUserFirstName.value = selectedUser.first_name;
+            selectedUserLastName.value =  selectedUser.last_name;
+            updatedUserFirstName.value = selectedUser.first_name;
+            updatedUserLastName.value = selectedUser.last_name;
+            updatedUserName.value = selectedUser.username;
     }
 
     function deleteUser(){
@@ -77,6 +145,10 @@
         selectedUserIndex.value = -1;
     }
     function editUser(){
-        editedUser.value = true;
+        const updateUser = users.value().filter((elem) => elem.id === selectedUserID.value)[0];
+        updateUser.first_name = updatedUserFirstName.value;
+        updateUser.last_name = updatedUserLastName.value;
+        updateUser.username = updatedUserName.value;
+        userState.dispatch('updateUser', updateUser);
     }
 </script>
