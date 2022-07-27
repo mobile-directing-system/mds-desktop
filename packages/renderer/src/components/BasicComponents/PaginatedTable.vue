c212fc57bc349a1ff89f693379a3d46873d6d93b
Yves Haas <yves.haas@uni-ulm.de>
2022-07-27 18:49:48 +0200

    feature: add paginated table component
    add a paginated table component and change editgroup view to explicitly
    load the user for each member. This fixes a problem where if there are
    more users than the specified amount in the retrieveUsers call then
    there is the possiblity of some users not being loaded for a given
    member id

---
+++ packages/renderer/src/components/BasicComponents/PaginatedTable.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class=" table-fixed place-items-center mr-10">
+    <table class=" border-spacing-2 w-full rounded-md overflow-hidden m-4">
+      <thead class=" bg-blue-700 font-bold text-white text-l text-left text-lg">
+        <tr>
+          <th 
+            v-for="title in props.columnTitles"
+            :key="title"
+          >
+            {{ title }}
+          </th>
+        </tr>
+      </thead> 
+      <tbody class="text-left">     
+        <tr
+          v-for="entity in props.contents" 
+          :key="entity[props.idIdentifier]"
+          class="border-b-2 border-b-gray-500 bg-white text-black hover:bg-primary_superlight cursor-pointer"
+          @click="$emit('click', entity[idIdentifier])"
+        >      
+          <td 
+            v-for="identifier in props.columnIdentifiers"
+            :key="identifier"
+          >
+            {{ entity[identifier] }}
+          </td>
+        </tr> 
+      </tbody>
+    </table>
+    <div class="flex justify-end">
+      <div class="flex gap-5">
+        <NormalButton
+          class="mt-4 ml-auto"
+          :btn-text="'Previous'"
+          :disabled="showPreviousButton? false:true"
+          @click="previousPage()"
+        />
+        {{ paginationPage + 1 }}/{{ paginationMaxPages }}
+        <NormalButton
+          class="mt-4 ml-auto"
+          :btn-text="'Next'"
+          :disabled="showNextButton? false:true"
+          @click="nextPage()"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+  
+  import { computed, ref, onMounted } from 'vue';
+  import NormalButton from './NormalButton.vue';
+
+  interface Props {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    contents: any[];
+    idIdentifier: string;
+    columnTitles: string[];
+    columnIdentifiers: string[];
+    totalRetrievableEntities: number;
+    initialPage?: number;
+  }
+  const props = defineProps<Props>();
+  // eslint-disable-next-line
+  const emit = defineEmits<{
+    (name: 'updatePage', {amount, offset}:{amount: number, offset: number}): void
+    (name: 'click', entityId: string): void
+  }>();
+
+  const paginationAmount = 5;
+  const paginationPage = ref(props.initialPage? props.initialPage : 0);
+  //const paginationPage = ref(0);
+  const paginationMaxPages = computed(() => Math.ceil(props.totalRetrievableEntities / paginationAmount ));
+  const showNextButton = computed(() => (paginationPage.value +1) < paginationMaxPages.value);
+  const showPreviousButton = computed(() => paginationPage.value > 0);
+
+  onMounted(() => {
+    updatePage();
+  });
+
+  async function updatePage() {
+    emit('updatePage', {amount: paginationAmount, offset: paginationPage.value * paginationAmount});
+  }
+
+  async function previousPage() {
+    paginationPage.value--;
+    updatePage();
+  }
+
+  async function nextPage() {
+    paginationPage.value++;
+    updatePage();
+  }
+
+</script>
--- packages/renderer/src/views/AllGroups.vue
+++ packages/renderer/src/views/AllGroups.vue
@@ -3,7 +3,7 @@
     <div class="grid bg-white  rounded-lg  my-10">
       <div class="flex justify-between">
         <h1 class=" ml-4 text-4xl font-bold text-black text-center">
-          All Users
+          All Groups
         </h1>
         <NormalButton
           class=" ml-auto mr-6"
@@ -11,65 +11,59 @@
           @click.prevent="router.push('/create-new-group')"
         />
       </div>
-      <div class=" table-fixed place-items-center mr-10">
-        <table class=" border-spacing-2 w-full rounded-md overflow-hidden m-4">
-          <thead class=" bg-blue-700 font-bold text-white text-l text-left text-lg">
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Operation</th>
-            </tr>
-          </thead> 
-          <tbody class="text-left">     
-            <tr
-              v-for="(group,i) in groups()" 
-              :key="i"
-              class="border-b-2 border-b-gray-500 bg-white text-black hover:bg-primary_superlight cursor-pointer"
-              @click="selectRow(i, group.id)"
-            >      
-              <td>{{ group.title }}</td>
-              <td>{{ group.description }}</td>
-              <td>{{ operations().filter((elem) => elem.id === group.operation)[0]?.title }}</td>
-            </tr> 
-          </tbody>
-        </table>
-        <div class="flex justify-between">
-          <NormalButton
-            class="mt-4 ml-auto"
-            :btn-text="'Cancel'"
-            @click="router.push('/main');"
-          />
-        </div>
+      <PaginatedTable 
+        :contents="contents"
+        id-identifier="id"
+        :column-titles="['Title', 'Description', 'Operation']"
+        :column-identifiers="['title', 'description', 'operation']"
+        :total-retrievable-entities="totalGroupAmount()"
+        @click="selectRow($event)"
+        @update-page="updatePage($event.amount, $event.offset)"
+      />
+      <div class="flex justify-between">
+        <NormalButton
+          class="mt-4 ml-auto"
+          :btn-text="'Cancel'"
+          @click="router.push('/main');"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup> 
-    import { ref, computed, onMounted } from 'vue';
+    import { computed } from 'vue';
     import NormalButton from '../components/BasicComponents/NormalButton.vue';
+    import PaginatedTable from '../components/BasicComponents/PaginatedTable.vue';
     import { useGroupState, useOperationsState } from '../store';
     import {useRouter} from 'vue-router';
 
-    onMounted(() => {
-      groupState.dispatch('retrieveGroups', {amount: 100});
-      operationsState.dispatch('retrieveOperations', {amount: 100});
-    });
+    //onMounted(async () => {
+    //  await groupState.dispatch('retrieveGroups', {amount: paginationAmount, offset: paginationPage.value * paginationAmount,});
+    //  groupPage.value().map((group) => operationsState.dispatch('retrieveOperation', group.operation ));
+    //});
 
+    //const paginationAmount = 5;
+    //const paginationPage = ref(0);
     const groupState = useGroupState();
     const operationsState = useOperationsState();
-    const groups = computed(() => groupState.getters.groups);
+    const groupPage = computed(() => groupState.getters.page);
+    const contents = computed(() => {
+      return [...groupPage.value()].map((elem) => {
+        const group = {...elem};
+        group.operation = operations.value().filter((elem) => elem.id === group.operation)[0]?.title;
+        return group;
+      });
+    });
+    const totalGroupAmount = computed(() => groupState.getters.total);
     const operations = computed(() => operationsState.getters.operations);
-    const selectedGroupIndex = ref(-1);
-    const selectedGroupID = ref('');
-    const selectedGroupTitle = ref('');
     const router = useRouter();
-    function selectRow(i: number, group_id: string){
-            selectedGroupIndex.value = i;
-            selectedGroupID.value = group_id;
-            const selectedGroup =groups.value().filter((elem) => elem.id === selectedGroupID.value)[0];
-            selectedGroupTitle.value = selectedGroup.title;
-            router.push({name: 'EditCurrentGroup', params:{ selectedGroupID: group_id}});
+    function selectRow(groupId: string){
+            router.push({name: 'EditCurrentGroup', params:{ selectedGroupID: groupId}});
+    }
+    async function updatePage(amount: number, offset: number) {
+      await groupState.dispatch('retrieveGroups', {amount, offset});
+      groupPage.value().map((group) => operationsState.dispatch('retrieveOperation', group.operation ));
     }
 </script>
 <style>
