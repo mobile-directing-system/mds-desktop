4c2fc5717e861815ec1a09a50486b53db95bd07f
Yves Haas <yves.haas@uni-ulm.de>
2022-09-14 16:01:00 +0200

    refactor(constants): add new permissions
    Add new permissions, delete no longer existing permissions, fix
    formatting and typos

--- packages/renderer/src/constants/index.ts
+++ packages/renderer/src/constants/index.ts
@@ -1,38 +1,50 @@
 // read-only permission strings in one place for quality of life reasons
 class PermissionNames {
-    // Permissions Permissions
-    static readonly PermissionsUpdate = 'permissions.update';
-    static readonly PermissionsView = 'permissions.view';
-    // Group Permissions
-    static readonly GroupCreate = 'group.create';
-    static readonly GroupUpdate = 'group.update';
-    static readonly GroupDelete = 'group.delete';
-    static readonly GroupView = 'group.view';
-    // Operations Permissions
-    static readonly OperationViewAny = 'operations.view.any';
-    static readonly OperationCreate = 'operations.create';
-    static readonly OperationUpdate = 'operations.update';
-    // Users Permissions
-    static readonly UserCreate = 'user.create';
-    static readonly UserDelete = 'user.delete';
-    static readonly UserUpdate = 'user.update';
-    static readonly UserUpdatePass = 'user.update-pass';
-    static readonly serSetAdmin = 'user.set-admin';
-    static readonly UserView = 'user.view';
+  // Permissions Permissions
+  public static readonly PermissionsUpdate = 'permissions.update';
+  public static readonly PermissionsView = 'permissions.view';
+  // Group Permissions
+  public static readonly GroupCreate = 'group.create';
+  public static readonly GroupUpdate = 'group.update';
+  public static readonly GroupDelete = 'group.delete';
+  public static readonly GroupView = 'group.view';
+  // Operations Permissions
+  public static readonly OperationViewAny = 'operations.view.any';
+  public static readonly OperationCreate = 'operations.create';
+  public static readonly OperationUpdate = 'operations.update';
+  public static readonly OperationMembersView = 'operations.members.view';
+  public static readonly OperationMembersUpdate = 'operations.members.update';
+  // Users Permissions
+  public static readonly UserCreate = 'user.create';
+  public static readonly UserUpdate = 'user.update';
+  public static readonly UserUpdatePass = 'user.update-pass';
+  public static readonly UserSetAdmin = 'user.set-admin';
+  public static readonly UserSetActive = 'user.set-active-state';
+  public static readonly UserView = 'user.view';
+  // Address Book Permissions
+  public static readonly AddressBookUpdateEntry = 'logistics.address-book.entry.update.any';
+  public static readonly AddressBookDeleteEntry = 'logistics.address-book.entry.delete.any';
+  public static readonly AddressBookViewEntry = 'logistics.address-book.entry.view.any';
+  // Intelligence Permissions
+  public static readonly IntelligenceCreate = 'intelligence.intel.create';
+  public static readonly IntelligenceInvalidate = 'intelligence.intel.invalidate';
+  public static readonly IntelligenceView = 'intelligence.intel.view';
+  //
+  public static readonly SearchRebuildIndex = 'core.search.rebuild-index';
 }
 
 // readonly strings for available order dirs
 class OrderDir {
-    static readonly Ascending = 'asc';
-    static readonly Descending = 'desc';
+  public static readonly Ascending = 'asc';
+  public static readonly Descending = 'desc';
 }
 
 // readonly strings for available order_by fields
 class OrderBy {
-    static readonly UserUsername = 'username';
-    static readonly UserFirstName = 'first_name';
-    static readonly UserLastname = 'last_name';
-    static readonly serIsAdmin = 'is_admin';
+  public static readonly UserUsername = 'username';
+  public static readonly UserFirstName = 'first_name';
+  public static readonly UserLastname = 'last_name';
+  public static readonly UserIsAdmin = 'is_admin';
 }
 
 export { PermissionNames, OrderDir, OrderBy };
