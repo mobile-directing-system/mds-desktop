2485502de74e1390e0ac3505cbc58df73b33cb5f
Yves Haas <yves.haas@uni-ulm.de>
2022-10-06 13:23:16 +0200

    refactor: remove hardcoded intel retrieval
    Remove the hardcoded retrieval of intel in the mailbox-page view

--- packages/renderer/src/views/MailboxPage.vue
+++ packages/renderer/src/views/MailboxPage.vue
@@ -12,10 +12,6 @@
   </div>
 </template>
 <script lang="ts" setup>
-  import { useIntelState } from '../store';
   import Mailbox from '../components/MailBox.vue';
-
-  const intelState = useIntelState();
-  intelState.dispatch('retrieveIntelById', 'bb2a2171-7ba0-40b3-9da7-09d52e31d330');
 
 </script>
