d7efe3ff7aa583d56c1d571fa195c8b3ad3623f9
Lennart Altenhof <l.f.altenhof@gmail.com>
2022-11-09 16:17:54 +0100

    test: fix broken karma test config

--- karma.conf.js
+++ karma.conf.js
@@ -41,5 +41,12 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+    },
+    failOnEmptyTestSuite: true,
   });
 };
