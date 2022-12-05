4a240eac78dbd75cb8d5d8761e2db445636bc815
Lennart Altenhof <l.f.altenhof@gmail.com>
2022-12-06 00:23:37 +0100

    feat: add delay for showing loading overlay
    Adds a delay before showing the loading overlay in order to avoid flickering.

--- src/app/core/util/loader.ts
+++ src/app/core/util/loader.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, finalize, Observable } from 'rxjs';
+import { BehaviorSubject, delay, finalize, Observable, of, Subscription, switchMap, tap } from 'rxjs';
 
 export class Loader {
   /**
@@ -42,5 +42,50 @@ export class Loader {
         this.emitLoadingChangeIfRequired();
       }),
     );
+  }
+
+  /**
+   * Registers the given {@link Observable} and adjusts {@link loadingChange} accordingly. Once the {@link Observable}
+   * is finished, the observable will emit accordingly again.
+   * @param o The Observable that performs some loading operation.
+   */
+  loadFrom<T>(o: () => Observable<T>): Observable<T> {
+    return of(void 0).pipe(
+      delay(0),
+      tap(() => {
+        this.inProgress++;
+        this.emitLoadingChangeIfRequired();
+      }),
+      switchMap(o),
+      delay(0),
+      finalize(() => {
+        this.inProgress--;
+        this.emitLoadingChangeIfRequired();
+      }),
+    )
+  }
+
+  private taken: { [keys: string]: Subscription } = {};
+
+  take(s: Subscription, key: string = ''): void {
+    this.inProgress++;
+    this.emitLoadingChangeIfRequired();
+    s.add(() => {
+      this.inProgress--;
+      this.emitLoadingChangeIfRequired();
+      if (key !== undefined) {
+        this.taken[key]?.unsubscribe();
+      }
+    });
+    if (key !== undefined) {
+      this.taken[key]?.unsubscribe();
+    }
+    // I currently don't know how the event loop is handled, but I hope that the finalizer from above is executed with
+    // the previous unsubscribe so that this does not overwrite and is then unintentionally unsubscribed.
+    this.taken[key] = s;
+  }
+
+  takeFrom(s: () => Subscription, key: string = '') {
+    this.take(s());
   }
 }
