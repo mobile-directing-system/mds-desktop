72d0e0945bd2e053e315c32ed1655b1e0ab778c0
ltodtuu <laurin.todt@uni-ulm.de>
2023-01-27 09:41:17 +0100

    feat: add edit address book entry view

---
+++ src/app/features/logistics/addressbook-logistics-view/edit-address-book-logistics-view/edit-address-book-logistics-view.component.spec.ts
@@ -0,0 +1,211 @@
+import { fakeAsync, tick } from '@angular/core/testing';
+
+import { EditAddressBookLogisticsView } from './edit-address-book-logistics-view.component';
+import { byTextContent, createRoutingFactory, SpectatorRouting, SpectatorRoutingOptions } from '@ngneat/spectator';
+import { LogisticsModule } from '../../logistics.module';
+import { NotificationService } from '../../../../core/services/notification.service';
+import { AddressBookService } from '../../../../core/services/addressbook.service';
+import { UserService } from '../../../../core/services/user.service';
+import { OperationService } from '../../../../core/services/operation.service';
+import { User } from '../../../../core/model/user';
+import { Operation } from '../../../../core/model/operation';
+import { firstValueFrom, of, throwError } from 'rxjs';
+import { SearchResult } from '../../../../core/util/store';
+
+function genFactoryOptions(): SpectatorRoutingOptions<EditAddressBookLogisticsView> {
+  return {
+    component: EditAddressBookLogisticsView,
+    imports: [
+      LogisticsModule,
+    ],
+    mocks: [
+      NotificationService,
+      AddressBookService,
+      UserService,
+      OperationService,
+    ],
+    params: {
+      entryId: 'develop',
+    },
+    detectChanges: false,
+  };
+}
+
+describe('EditAddressBookLogisticsView', () => {
+  let component: EditAddressBookLogisticsView;
+  let spectator: SpectatorRouting<EditAddressBookLogisticsView>;
+  const createComponent = createRoutingFactory(genFactoryOptions());
+
+  const label = 'pipe';
+  const description = 'remark';
+  const operationId = 'parcel';
+  const userId = 'hard';
+
+  const sampleUserData: User[] = [
+    {
+      id: 'hard',
+      username: 'marry',
+      firstName: 'well',
+      lastName: 'forgive',
+      isActive: true,
+      isAdmin: false,
+    },
+    {
+      id: 'glass',
+      username: 'everyday',
+      firstName: 'robbery',
+      lastName: 'beak',
+      isActive: true,
+      isAdmin: false,
+    },
+    {
+      id: 'combine',
+      username: 'greet',
+      firstName: 'swear',
+      lastName: 'real',
+      isActive: false,
+      isAdmin: false,
+    },
+  ];
+
+  const sampleOperation: Operation = {
+    id: 'parcel',
+    title: 'roof',
+    description: 'temple',
+    start: new Date(),
+    is_archived: false,
+  };
+
+  beforeEach(async () => {
+    spectator = createComponent();
+    component = spectator.component;
+    spectator.router.navigate = jasmine.createSpy().and.callFake(spectator.router.navigate).and.resolveTo();
+    spectator.inject(UserService).getUserById.withArgs('hard').and.returnValue(of(sampleUserData[0]));
+    spectator.inject(UserService).getUserById.withArgs('glass').and.returnValue(of(sampleUserData[1]));
+    spectator.inject(UserService).getUserById.withArgs('combine').and.returnValue(of(sampleUserData[2]));
+    spectator.inject(UserService).searchUsers.and.returnValue(of(new SearchResult(sampleUserData, {
+      estimatedTotalHits: 3,
+      offset: 0,
+      limit: 5,
+      query: '',
+      processingTime: 10,
+    })));
+    spectator.inject(OperationService).getOperationById.and.returnValue(of(sampleOperation));
+    spectator.inject(OperationService).searchOperations.and.returnValue(of(new SearchResult([sampleOperation], {
+      estimatedTotalHits: 1,
+      offset: 0,
+      limit: 5,
+      query: '',
+      processingTime: 10,
+    })));
+    spectator.inject(AddressBookService).getAddressBookEntryById.and.returnValue(of({
+      id: 'develop',
+      label: label,
+      description: description,
+      operation: operationId,
+      user: userId,
+    }))
+    spectator.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('updateEntry', () => {
+    it('should update address book entry correctly', fakeAsync(() => {
+      component.form.patchValue({
+        label: label,
+        description: description,
+        operation: operationId,
+        user: userId,
+      });
+      tick();
+
+      spectator.inject(AddressBookService).updateAddressBookEntry.and.returnValue(of(void 0));
+
+      component.updateEntry();
+      tick();
+      expect(spectator.inject(AddressBookService).updateAddressBookEntry).toHaveBeenCalledOnceWith({
+        id: component.entryId,
+        label: label,
+        description: description,
+        operation: operationId,
+        user: userId,
+      });
+    }));
+
+    it('should should show error when updating address book entry failed', fakeAsync(() => {
+      component.form.setValue({
+        label: label,
+        description: description,
+        operation: operationId,
+        user: userId,
+      });
+      tick();
+
+      spectator.inject(AddressBookService).updateAddressBookEntry.and.returnValue(throwError(() => ({})));
+
+      component.updateEntry();
+      tick();
+      expect(spectator.inject(NotificationService).notifyUninvasiveShort).toHaveBeenCalled();
+    }));
+  });
+
+  describe('searchUsers', () => {
+    it('should call UserService and return correct value', fakeAsync(async () => {
+      const result = await firstValueFrom(component.searchUsers(''));
+      expect(result).toEqual(sampleUserData);
+
+      expect(spectator.inject(UserService).searchUsers).toHaveBeenCalledWith({
+        query: '',
+        limit: 5,
+        offset: 0,
+      }, false);
+    }));
+  });
+
+  describe('searchOperations', () => {
+    it('should call OperationService and return correct value', fakeAsync(async () => {
+      let searchOperationsResult = await firstValueFrom(component.searchOperations(''));
+      expect(searchOperationsResult).toEqual([sampleOperation]);
+      expect(spectator.inject(OperationService).searchOperations).toHaveBeenCalledWith({
+        query: '',
+        limit: 5,
+        offset: 0,
+      }, {});
+    }));
+  });
+
+  describe('fixture', () => {
+    it('should show update button, when updating is allowed', fakeAsync(async () => {
+      component.form.setValue({
+        label: label,
+        description: description,
+        operation: operationId,
+        user: userId,
+      });
+      tick();
+      spectator.detectComponentChanges();
+      await spectator.fixture.whenStable();
+      expect(spectator.query(byTextContent('Update', { selector: 'button' }))).not.toBeDisabled();
+    }));
+
+    it('should update address book entry, when update button is clicked', fakeAsync(async () => {
+      component.form.setValue({
+        label: label,
+        description: description,
+        operation: operationId,
+        user: userId,
+      });
+      tick();
+      spectator.detectComponentChanges();
+      await spectator.fixture.whenStable();
+      spyOn(component, 'updateEntry');
+
+      spectator.click(byTextContent('Update', { selector: 'button' }));
+
+      expect(component.updateEntry).toHaveBeenCalledOnceWith();
+    }));
+  });
+});
---
+++ src/app/features/logistics/addressbook-logistics-view/edit-address-book-logistics-view/edit-address-book-logistics-view.component.ts
@@ -0,0 +1,153 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, Subscription, switchMap } from 'rxjs';
+import { AddressBookService } from '../../../../core/services/addressbook.service';
+import { NotificationService } from '../../../../core/services/notification.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../../../../core/services/user.service';
+import { OperationService } from '../../../../core/services/operation.service';
+import { Loader } from '../../../../core/util/loader';
+import { User } from '../../../../core/model/user';
+import { Operation } from '../../../../core/model/operation';
+import { map } from 'rxjs/operators';
+import { SearchResult } from '../../../../core/util/store';
+import { MDSError, MDSErrorCode } from '../../../../core/util/errors';
+
+/**
+ * View to edit an existing {@link AddressBookEntry}
+ */
+@Component({
+  selector: 'app-edit-address-book-logistics-view',
+  templateUrl: './edit-address-book-logistics-view.component.html',
+  styleUrls: ['./edit-address-book-logistics-view.component.scss'],
+})
+export class EditAddressBookLogisticsView implements OnInit, OnDestroy {
+
+  loader = new Loader();
+  entryId = '';
+  s: Subscription[] = [];
+  userDetails: User | undefined | null;
+
+  constructor(private addressBookService: AddressBookService, private notificationService: NotificationService, private fb: FormBuilder,
+              private router: Router, private route: ActivatedRoute, private userService: UserService, private operationService: OperationService) {
+  }
+
+
+  ngOnDestroy(): void {
+    this.s.forEach(s => s.unsubscribe());
+  }
+
+  ngOnInit(): void {
+    this.s.push(this.route.params.pipe(
+      switchMap(params => {
+        this.entryId = params['entryId'];
+        return this.loader.load(this.addressBookService.getAddressBookEntryById(this.entryId));
+      }),
+    ).subscribe(currentEntry => {
+      // patch the details of the current Entry.
+      this.form.patchValue({
+        label: currentEntry.label,
+        description: currentEntry.description,
+        operation: currentEntry.operation,
+        user: currentEntry.user,
+      });
+      this.userDetails = currentEntry.userDetails;
+    }));
+
+    this.s.push(
+      this.form.controls.user.valueChanges.pipe(
+        switchMap(userId => {
+          if (!userId){
+            return of(null);
+          }
+          return this.userService.getUserById(userId);
+        })
+      ).subscribe(userDetails => {
+        this.userDetails = userDetails;
+      })
+    )
+  }
+
+  form = this.fb.nonNullable.group({
+    label: this.fb.nonNullable.control<string>('', Validators.required),
+    description: this.fb.nonNullable.control<string>(''),
+    operation: this.fb.nonNullable.control<string | null>(null),
+    user: this.fb.nonNullable.control<string | null>(null),
+  });
+
+  updateEntry(): void {
+    const label = this.form.value.label;
+    if (label === undefined) {
+      throw new MDSError(MDSErrorCode.AppError, 'label control is not set.');
+    }
+    const description = this.form.value.description;
+    if (description === undefined) {
+      throw new MDSError(MDSErrorCode.AppError, 'description control is not set.');
+    }
+    const operation = this.form.value.operation;
+    if (operation === undefined) {
+      throw new MDSError(MDSErrorCode.AppError, 'operation control is not set.');
+    }
+    const user = this.form.value.user;
+    if (user === undefined) {
+      throw new MDSError(MDSErrorCode.AppError, 'user control is not set.');
+    }
+    this.loader.load(this.addressBookService.updateAddressBookEntry({
+      id: this.entryId,
+      label: label,
+      description: description,
+      operation: operation ?? undefined,
+      user: user ?? undefined,
+    })).subscribe({
+      next: _ => {
+        this.cancel();
+      },
+      error: _ => {
+        this.notificationService.notifyUninvasiveShort($localize`Updating address book entry failed.`);
+      },
+    });
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.userService.getUserById(id);
+  }
+
+  getOperationById(id: string): Observable<Operation> {
+    return this.operationService.getOperationById(id);
+  }
+
+  asUser(entity: User): User {
+    return entity;
+  }
+
+  asOperation(entity: Operation): Operation {
+    return entity;
+  }
+
+  searchUsers(query: string): Observable<User[]> {
+    return this.userService.searchUsers({
+      query: query,
+      limit: 5,
+      offset: 0,
+    }, false).pipe(
+      map((res: SearchResult<User>): User[] => {
+        return res.hits;
+      }));
+  }
+
+  searchOperations(query: string): Observable<Operation[]> {
+    return this.operationService.searchOperations({
+        query: query,
+        limit: 5,
+        offset: 0,
+      }, {},
+    ).pipe(
+      map((res: SearchResult<Operation>): Operation[] => {
+        return res.hits;
+      }));
+  }
+
+  cancel() {
+    this.router.navigate(['..'], { relativeTo: this.route }).then();
+  }
+}
