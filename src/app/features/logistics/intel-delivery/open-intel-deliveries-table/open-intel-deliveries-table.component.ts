e00e95b999da2649014bbd19a2d9665b9879703b
Marvin <65172043+MMV-pro@users.noreply.github.com>
2023-05-17 13:01:45 +0200

    210-change-impove-usability-of-intel-delivery (#211)
    closes #210 
    
    * change: highlighted selected open intel delivery in table
    
    * change: moved highlighting a level up for better support. removed operation from tables
    
    * change: adapted test

--- src/app/features/logistics/intel-delivery/open-intel-deliveries-table/open-intel-deliveries-table.component.ts
+++ src/app/features/logistics/intel-delivery/open-intel-deliveries-table/open-intel-deliveries-table.component.ts
@@ -19,12 +19,16 @@ export class OpenIntelDeliveriesTableComponent {
     }
   }
 
+  @Input()
+  selected?: DetailedOpenIntelDelivery;
+
   /**
    * Emits when an open delivery is selected. The emitted value is the delivery id.
    */
   @Output() deliverySelected = new EventEmitter<string>();
+  deliverSelectedId? : string;
 
-  readonly columns = ['intelCreatedAt', 'intelOperation', 'intelCreatedBy', 'deliveryTo', 'intelType', 'intelImportance'];
+  readonly columns = ['intelCreatedAt', 'intelCreatedBy', 'deliveryTo', 'intelType', 'intelImportance'];
 
   asDetailedOpenIntelDelivery(d: DetailedOpenIntelDelivery): DetailedOpenIntelDelivery {
     return d;
@@ -35,6 +39,7 @@ export class OpenIntelDeliveriesTableComponent {
    * @param deliveryId The id of the delivery to select.
    */
   selectOpenIntelDelivery(deliveryId: string): void {
+    this.deliverSelectedId = deliveryId;
     this.deliverySelected.next(deliveryId);
   }
 }
