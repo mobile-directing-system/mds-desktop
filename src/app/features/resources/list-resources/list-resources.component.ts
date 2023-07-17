5899a27e87513c77e515b1c0b9deed83b8a11bda
Lars Thomae <lars.thomae@aol.com>
2023-07-17 15:06:37 +0200

    Finish resource table

--- src/app/features/resources/list-resources/list-resources.component.ts
+++ src/app/features/resources/list-resources/list-resources.component.ts
@@ -1,10 +1,8 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { map } from 'rxjs';
+import { concatMap, from, map, mergeMap, of, toArray } from 'rxjs';
 import { OperationService } from 'src/app/core/services/operation.service';
 import { ResourceService } from 'src/app/core/services/resource/resource.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -22,8 +20,6 @@ interface ResourceRow {
   selector: 'app-list-resources',
   templateUrl: './list-resources.component.html',
   styleUrls: ['./list-resources.component.scss'],
-  standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
 })
 export class ListResourcesComponent implements AfterViewInit {
   displayedColumns: string[] = ['label', 'description', 'user', 'operation', 'incident', 'status'];
@@ -37,19 +33,40 @@ export class ListResourcesComponent implements AfterViewInit {
   }
 
   refreshDataSource() {
-    let resourceRows = this.resourceService.getAllResources().pipe(map(resource => {
+    let resourceRows = this.resourceService.getAllResources().pipe(
+      concatMap(resources => from(resources)),
+      map((resource, _) => {
+        let resourceRow = <ResourceRow>({
+          label: resource.label,
+          description: resource.description,
+          user: resource.user,
+          operation: resource.operation,
+          incident: "n.a.",
+          status: resource.statusCode?.toString()
+        });
+        if(resource.operation) this.operationService.getOperationById(resource.operation).subscribe(operation => resourceRow.operation = operation.title);
+        if(resource.user) this.userService.getUserById(resource.user).subscribe(user => resourceRow.user = user.username);
+        return resourceRow;
+      }),
+      toArray());
 
-      return {
+    resourceRows.subscribe(rows => {
+      this.dataSource = new MatTableDataSource<ResourceRow>(rows);
+      console.log(rows);
+    });
+  }
 
-      };
-    }));
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-
-
-
 }
