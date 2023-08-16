ace29a760d0e692e9da68ef9f191f984c400c210
Lars <63348681+AtLars@users.noreply.github.com>
2023-08-16 15:16:39 +0200

    Fix: Display incidents on resource page (#234)

--- src/app/features/resources/list-resources/list-resources.component.ts
+++ src/app/features/resources/list-resources/list-resources.component.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { concatMap, from, map, mergeMap, of, toArray } from 'rxjs';
+import { concatMap, from, map, toArray } from 'rxjs';
+import { IncidentService } from 'src/app/core/services/incident/incident.service';
 import { OperationService } from 'src/app/core/services/operation.service';
 import { ResourceService } from 'src/app/core/services/resource/resource.service';
 import { UserService } from 'src/app/core/services/user.service';
@@ -30,7 +31,8 @@ export class ListResourcesComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private resourceService: ResourceService, private operationService: OperationService, private userService: UserService, private router: Router) {
+  constructor(private resourceService: ResourceService, private operationService: OperationService,
+    private userService: UserService, private incidentService: IncidentService, private router: Router) {
     this.refreshDataSource();
   }
 
@@ -47,8 +49,9 @@ export class ListResourcesComponent implements AfterViewInit {
           incident: "",
           status: resource.statusCode?.toString()
         });
-        if(resource.operation) this.operationService.getOperationById(resource.operation).subscribe(operation => resourceRow.operation = operation.title);
-        if(resource.user) this.userService.getUserById(resource.user).subscribe(user => resourceRow.user = user.username);
+        if (resource.operation) this.operationService.getOperationById(resource.operation).subscribe(operation => resourceRow.operation = operation.title);
+        if (resource.user) this.userService.getUserById(resource.user).subscribe(user => resourceRow.user = user.username);
+        if(resource.incident) this.incidentService.getIncidentById(resource.incident).subscribe(incident => resourceRow.incident = incident?.name ?? "");
         return resourceRow;
       }),
       toArray());
