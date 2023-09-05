55b5aec9818ae5a21224ee64632e2691231d2a58
Lars <63348681+AtLars@users.noreply.github.com>
2023-09-05 13:13:57 +0200

    Feature: Add message service (#241)
    Adds message service
    Closes #238

---
+++ src/app/core/model/message.ts
@@ -0,0 +1,103 @@
+import { ChannelType } from "./channel";
+
+/**
+ * Participants of the system that are adressable in a message
+ */
+export enum Participant {
+    AddressBookEntry, Resource, Role
+}
+
+/**
+ * Direction of a message
+ */
+export enum MessageDirection {
+    Incoming, Outgoing
+}
+
+/**
+ * Recipient of a message
+ */
+export interface Recipient {
+    /**
+     * A recipient can be an address book entry, a resource or a role
+     */
+    recipientType: Participant;
+    /**
+     * The id of the recipient
+     */
+    recipientId: string;
+    /**
+     * The reviewer must select a channel for the recipient when
+     * the {@link recipientType} is an address book entry or a resource
+     */
+    channelId?: string;
+    /**
+     * Whether the recipient already read the messsage.
+     * This is only relevant for {@link Participant.Role} in the system
+     * because for other participants the read status cannot be determined.
+     */
+    read?: boolean;
+    /**
+     * Whether the message was send to the recipient
+     */
+    send?: boolean;
+}
+
+/**
+ * Represents an incoming or an outgoing message in the system
+ */
+export interface Message {
+    /**
+     * Identifies the message.
+     */
+    id: string;
+    /**
+     * Signals if the message is incoming or outgoing
+     */
+    direction: MessageDirection;
+    /**
+     * Type of an incoming message (e.g. radio or telephone)
+     */
+    incomingChannelType?: ChannelType;
+    /**
+     * A sender can be an address book entry, a resource or a role
+     */
+    senderType?: Participant;
+    /**
+     * Id of the sender
+     */
+    senderId?: string;
+    /**
+     * Additional information about the sender (e.g. mail address, telephone number)
+     */
+    info?: string;
+    /**
+     * Content of the messages
+     */
+    content: string;
+    /**
+     * Incident the message is associated with
+     */
+    incidentId?: string;
+    /**
+     * Only if the {@link senderType} is a {@link Participant.Resource}:
+     * New status code of the resource
+     */
+    resourceStatusCode?: number;
+    /**
+     * Timestamp when the message was created
+     */
+    createdAt: Date;
+    /**
+     * Signals if an incoming message needs a review from the reviewer
+     */
+    needsReview?: boolean;
+    /**
+     * Priority of the message
+     */
+    priority?: number;
+    /**
+     * Recipients of the message
+     */
+    recipients: Recipient[];
+}
---
+++ src/app/core/services/message/local-storage-message.service.spec.ts
@@ -0,0 +1,158 @@
+import { TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
+
+import { LocalStorageMessageService } from './local-storage-message.service';
+import { MessageService } from './message.service';
+import { Message, MessageDirection, Participant } from '../../model/message';
+import { ChannelType } from '../../model/channel';
+import { mockLocalStorage } from '../../util/testing';
+
+fdescribe('LocalStorageMessageService', () => {
+  let service: MessageService;
+
+  const exampleMessages: Message[] = [
+    {
+      id: "0",
+      direction: MessageDirection.Incoming,
+      incomingChannelType: ChannelType.Email,
+      senderId: "123",
+      senderType: Participant.AddressBookEntry,
+      content: "Example content",
+      createdAt: new Date(),
+      recipients: [
+        {
+          recipientType: Participant.Role,
+          recipientId: "S1",
+          read: false
+        }
+      ]
+    },
+    {
+      id: "1",
+      direction: MessageDirection.Incoming,
+      incomingChannelType: ChannelType.Email,
+      senderId: "1234",
+      senderType: Participant.Resource,
+      content: "Example content 123",
+      createdAt: new Date(),
+      needsReview: true,
+      recipients: [
+        {
+          recipientType: Participant.Role,
+          recipientId: "S1",
+          read: false
+        },
+        {
+          recipientType: Participant.Role,
+          recipientId: "S3",
+          read: false
+        }
+      ]
+    },
+    {
+      id: "2",
+      direction: MessageDirection.Outgoing,
+      senderType: Participant.Role,
+      senderId: "S1",
+      content: "A message from S1",
+      createdAt: new Date(),
+      needsReview: true,
+      recipients: [
+        {
+          recipientType: Participant.Role,
+          recipientId: "S2",
+          read: false
+        },
+        {
+          recipientType: Participant.Role,
+          recipientId: "S3",
+          read: false
+        }
+      ]
+    },
+    {
+      id: "3",
+      direction: MessageDirection.Outgoing,
+      senderType: Participant.Role,
+      senderId: "S2",
+      content: "A message from S2",
+      createdAt: new Date(),
+      recipients: [
+        {
+          recipientType: Participant.Role,
+          recipientId: "S4",
+          read: false
+        },
+        {
+          recipientType: Participant.AddressBookEntry,
+          recipientId: "12345",
+          send: false
+        },
+      ]
+    }
+  ];
+
+  beforeEach(() => {
+    mockLocalStorage();
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: MessageService,
+          useClass: LocalStorageMessageService
+        }
+      ]
+    });
+    service = TestBed.inject(MessageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should fetch all items correctly', fakeAsync(()=> {
+    service.createMessage(exampleMessages[0]).subscribe();
+    service.createMessage(exampleMessages[1]).subscribe();
+    tick();
+    service.getMessages().subscribe(messages => {
+      expect(messages[0].senderId).toBe(exampleMessages[0].senderId);
+      expect(messages[1].senderId).toBe(exampleMessages[1].senderId);
+    });
+  }));
+
+  it('should fetch messages correctly with byDirection filter', fakeAsync(() => {
+    for(let msg of exampleMessages) {
+      service.createMessage(msg).subscribe();
+    }
+    tick();
+    service.getMessages({byDirection: MessageDirection.Incoming}).subscribe(messages => {
+      expect(messages.length).toBe(2);
+      expect(messages[0].direction).toBe(MessageDirection.Incoming);
+      expect(messages[1].direction).toBe(MessageDirection.Incoming);
+    });
+  }));
+
+  it('should fetch messages correctly with byNeedsReview filter', fakeAsync(() => {
+    for(let msg of exampleMessages) {
+      service.createMessage(msg).subscribe();
+    }
+    tick();
+    service.getMessages({byNeedsReview: true}).subscribe(messages => {
+      expect(messages.length).toBe(2);
+      for(let msg of messages) {
+        expect(msg.needsReview).toBeTrue();
+      }
+    });
+  }));
+
+  it('should fetch messages for mailbox that are not read correctly', fakeAsync(() => {
+    for(let msg of exampleMessages) {
+      service.createMessage(msg).subscribe();
+    }
+    tick();
+    service.getMailboxMessages("S1", false).subscribe(messages => {
+      expect(messages.length).toBe(2);
+      for(let msg of messages) {
+        expect(msg.recipients).toContain(jasmine.objectContaining({recipientId: "S1"}));
+      }
+    });
+  }));
+});
