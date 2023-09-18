ae3e814bac3b0c59ecbdb924973ca973ada60e3f
Lars Thomae <lars.thomae@uni-ulm.de>
2023-09-18 12:27:32 +0200

    Add table for outgoing messages

--- src/app/features/reviewer/reviewer.module.ts
+++ src/app/features/reviewer/reviewer.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreModule } from 'src/app/core/core.module';
+import { ReviewerOutgoingView } from './outgoing/reviewer-outgoing-view.component';
 
 
 
 @NgModule({
   declarations: [
+    ReviewerOutgoingView
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule
   ]
 })
 export class ReviewerModule { }
