f58575f4c899ad381bb9b6902704f0dfb4cefaf2
Marvin Voelter <marvin.voelter@uni-ulm.de>
2023-09-29 13:11:39 +0200

    feat: adding signaler outgoing page to allow a signaler to see delivery jobs and mark it as done after sending it

--- src/app/features/signaler/outgoing/delivery-item/delivery-item.component.ts
+++ src/app/features/signaler/outgoing/delivery-item/delivery-item.component.ts
@@ -1,14 +1,9 @@
-import {Component, EventEmitter, Inject, Input, Output} from '@angular/core';
-import {DetailedRadioDelivery, RadioDelivery} from "../../../../core/model/radio-delivery";
+import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {DialogData} from "../../../mailbox/incoming-messages-view/incoming-messages-view.component";
-import {MessageService} from "../../../../core/services/message/message.service";
-import {NotificationService} from "../../../../core/services/notification.service";
-import {Router} from "@angular/router";
 import {DeliveryItemDialogAction, DeliveryItemDialogData} from "../signaler-outgoing-view.component";
 
 /**
- * Component for visualising one {@link RadioDelivery}
+ * Component for visualising a {@link Message} which should be sent
  */
 @Component({
   selector: 'app-delivery-item',
@@ -19,17 +14,13 @@ export class DeliveryItemComponent {
 
   constructor(
     public dialogRef: MatDialogRef<DeliveryItemComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DeliveryItemDialogData,
-    public messageService: MessageService,
-    private notificationService: NotificationService,
-    private router: Router
+    @Inject(MAT_DIALOG_DATA) public data: DeliveryItemDialogData
   ) {}
 
   /**
-   * Closes dialog
+   * Closes the dialog
    */
   closeDialog(action: DeliveryItemDialogAction): void {
-    console.log("MMV closed")
     this.dialogRef.close(action);
   }
 
