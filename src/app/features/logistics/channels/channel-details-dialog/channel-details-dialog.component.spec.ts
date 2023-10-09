239fc71ed76a3b19ba3bbb28d1accaefd94beaf7
Lars <63348681+AtLars@users.noreply.github.com>
2023-10-09 12:53:32 +0200

    change: remove channel form fields that are no longer needed
    Closes #261

--- src/app/features/logistics/channels/channel-details-dialog/channel-details-dialog.component.spec.ts
+++ src/app/features/logistics/channels/channel-details-dialog/channel-details-dialog.component.spec.ts
@@ -1,24 +1,22 @@
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { fakeAsync, tick } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
+import { MatSelectHarness } from '@angular/material/select/testing';
+import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
+import { Spectator, byTextContent, createComponentFactory } from '@ngneat/spectator';
+import * as moment from 'moment';
+import { CoreModule } from '../../../../core/core.module';
+import { ChannelType, localizeChannelType } from '../../../../core/model/channel';
+import { Importance } from '../../../../core/model/importance';
+import { newMatDialogRefMock } from '../../../../core/testutil/testutil';
+import { LogisticsModule } from '../../logistics.module';
 import {
   ChannelDetailsDialog,
   ChannelDetailsDialogData,
   ChannelDetailsDialogResult,
 } from './channel-details-dialog.component';
-import { byTextContent, createComponentFactory, Spectator } from '@ngneat/spectator';
-import { CoreModule } from '../../../../core/core.module';
-import { LogisticsModule } from '../../logistics.module';
-import { ChannelType, localizeChannelType } from '../../../../core/model/channel';
-import * as moment from 'moment';
-import { Importance, localizeImportance } from '../../../../core/model/importance';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { HarnessLoader } from '@angular/cdk/testing';
-import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
-import { MatFormFieldHarness } from '@angular/material/form-field/testing';
-import { fakeAsync, tick } from '@angular/core/testing';
-import { MatSelectHarness } from '@angular/material/select/testing';
-import { MatInputHarness } from '@angular/material/input/testing';
-import { formatEditDuration } from '../../../../core/components/duration-picker/duration-picker.component';
-import { newMatDialogRefMock } from '../../../../core/testutil/testutil';
-import { MatSlideToggleHarness } from '@angular/material/slide-toggle/testing';
 
 describe('ChannelDetailsDialog', () => {
   let spectator: Spectator<ChannelDetailsDialog>;
@@ -90,10 +88,7 @@ describe('ChannelDetailsDialog', () => {
 
   [
     'label',
-    'channel type',
-    'importance',
-    'priority',
-    'timeout',
+    'channel type'
   ].forEach(label => {
     it(`should display form field for ${ label }`, async () => {
       const labels = await Promise.all(formFields.map(ff => ff.getLabel()));
@@ -180,142 +175,6 @@ describe('ChannelDetailsDialog', () => {
       });
       await spectator.fixture.whenStable();
       expect(component.form.getRawValue().type).toEqual(newChannelType);
-    });
-  });
-
-  describe('min importance form field', () => {
-    let minImportanceSelect: MatSelectHarness;
-    const newMinImportance: Importance = Importance.Low;
-    const searchOption = 'low';
-
-    beforeEach(async () => {
-      const formField = spectator.query(byTextContent('Importance', {
-        exact: false,
-        selector: 'mat-form-field',
-      }));
-      if (!formField) {
-        return;
-      }
-      formField.className += ' TEST_SELECT';
-      const tmpImportanceSelect = await harnessLoader.getHarnessOrNull(MatSelectHarness.with({
-        ancestor: '.TEST_SELECT',
-      }));
-      if (!tmpImportanceSelect) {
-        return;
-      }
-      minImportanceSelect = tmpImportanceSelect;
-    });
-
-    it('should display select', () => {
-      expect(minImportanceSelect).toBeTruthy();
-    });
-
-    it('should display value', async () => {
-      const selected = await minImportanceSelect.getValueText();
-      const search = localizeImportance(dialogData.channel.minImportance);
-      expect(selected.includes(search))
-        .withContext(`expect ${ search } in ${ selected }`).toBeTrue();
-    });
-
-    it('should update importance on selection', async () => {
-      await minImportanceSelect.open();
-      const options = await minImportanceSelect.getOptions();
-      const optionLabels = await Promise.all(options.map(o => o.getText()));
-      const optionIndex = optionLabels.findIndex(l => l.toLowerCase().includes(searchOption));
-      if (optionIndex === -1) {
-        fail(`option '${ searchOption }' not included in available ones: ${ optionLabels }`);
-        return;
-      }
-      const optionText = await options[optionIndex].getText();
-      await minImportanceSelect.clickOptions({
-        text: optionText,
-      });
-      await spectator.fixture.whenStable();
-      expect(component.form.getRawValue().minImportance).toEqual(newMinImportance);
-    });
-  });
-
-  describe('priority form field', () => {
-    let priorityInput: HTMLInputElement;
-    const newPriority = 135;
-
-    beforeEach(() => {
-      const tmpLabelInput = spectator.query(byTextContent('Priority', {
-        exact: false,
-        selector: 'mat-form-field',
-      }))?.querySelector('input');
-      if (tmpLabelInput) {
-        priorityInput = tmpLabelInput;
-      }
-    });
-
-    it('should display input', () => {
-      expect(priorityInput).toBeTruthy();
-    });
-
-    it('should display value', () => {
-      const value = priorityInput.value;
-      expect(value).toEqual(`${ dialogData.channel.priority }`);
-    });
-
-    it('should update priority when input changes', fakeAsync(() => {
-      spectator.typeInElement(`${ newPriority }`, priorityInput);
-      tick();
-      expect(component.form.getRawValue().priority).toEqual(newPriority);
-    }));
-  });
-
-  describe('timeout form field', () => {
-    let formField: MatFormFieldHarness;
-    let timeoutInput: MatInputHarness;
-    const newTimeout = moment.duration({
-      hours: 23,
-      minutes: 23,
-      seconds: 1,
-      milliseconds: 340,
-    });
-
-    beforeEach(async () => {
-      const tmpFormField = spectator.query(byTextContent('Timeout', {
-        exact: false,
-        selector: 'mat-form-field',
-      }));
-      if (!tmpFormField) {
-        return;
-      }
-      tmpFormField.className += ' TEST_SELECT';
-      formField = await harnessLoader.getHarness(MatFormFieldHarness.with({
-        selector: '.TEST_SELECT',
-      }));
-      const tmpTimeoutInput = await harnessLoader.getHarnessOrNull(MatInputHarness.with({
-        ancestor: '.TEST_SELECT',
-      }));
-      if (!tmpTimeoutInput) {
-        return;
-      }
-      timeoutInput = tmpTimeoutInput;
-    });
-
-    it('should display input', () => {
-      expect(timeoutInput).toBeTruthy();
-    });
-
-    it('should display value', async () => {
-      const value = await timeoutInput.getValue();
-      expect(value).toEqual(formatEditDuration(dialogData.channel.timeout));
-    });
-
-    it('should update timeout on input change', async () => {
-      await timeoutInput.setValue(formatEditDuration(newTimeout));
-      await spectator.fixture.whenStable();
-      expect(component.form.getRawValue().timeout).toEqual(newTimeout);
-    });
-
-    it('should mark negative time as invalid', async () => {
-      await timeoutInput.setValue('-1s');
-      await spectator.fixture.whenStable();
-      const isValid = await formField.isControlValid();
-      expect(isValid).toBeFalse();
     });
   });
 
