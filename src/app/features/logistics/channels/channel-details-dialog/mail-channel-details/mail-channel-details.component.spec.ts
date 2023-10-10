98e779af057d1b4b064dc3ef1b4db86d2416e89e
Lars Thomae <lars.thomae@aol.com>
2023-10-10 12:46:06 +0200

    tests: add tests for email channel details

--- src/app/features/logistics/channels/channel-details-dialog/mail-channel-details/mail-channel-details.component.spec.ts
+++ src/app/features/logistics/channels/channel-details-dialog/mail-channel-details/mail-channel-details.component.spec.ts
@@ -1,21 +1,82 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
+import { MatInputHarness } from '@angular/material/input/testing';
+import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { CoreModule } from 'src/app/core/core.module';
+import { MailChannelDetails } from 'src/app/core/model/channel';
+import { LogisticsModule } from '../../../logistics.module';
 import { MailChannelDetailsComponent } from './mail-channel-details.component';
 
-describe('MailChannelDetailsComponent', () => {
-  let component: MailChannelDetailsComponent;
-  let fixture: ComponentFixture<MailChannelDetailsComponent>;
+@Component({
+  template: `
+    <app-mail-channel-details [formControl]="fc"></app-mail-channel-details>`,
+})
+class TestHostComponent {
+  fc = new FormControl<MailChannelDetails>({
+    email: '',
+  }, { nonNullable: true });
+}
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [MailChannelDetailsComponent]
-    });
-    fixture = TestBed.createComponent(MailChannelDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+fdescribe('MailChannelDetailsComponent', () => {
+  let spectator: Spectator<TestHostComponent>;
+  let host: TestHostComponent;
+  let hLoader: HarnessLoader;
+
+  let details: MailChannelDetails = {
+    email: "test@example.com"
+  };
+  
+  const createComponent = createComponentFactory({
+    component: TestHostComponent,
+    imports: [CoreModule, LogisticsModule],
+    declarations: [MailChannelDetailsComponent],
+    detectChanges: false,
+  });
+
+  beforeEach(async () => {
+    spectator = createComponent();
+    host = spectator.component;
+    host.fc.setValue(details);
+    spectator.detectComponentChanges();
+    await spectator.fixture.whenStable();
+    hLoader = await TestbedHarnessEnvironment.loader(spectator.fixture);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(host).toBeTruthy();
+  });
+
+  describe('email', () => {
+    let mailFF: MatFormFieldHarness;
+    let input: MatInputHarness;
+
+    beforeEach(async () => {
+      mailFF = await hLoader.getHarness(MatFormFieldHarness.with({
+        floatingLabelText: 'Email',
+      }));
+      input = await hLoader.getHarness(MatInputHarness.with({
+        value: host.fc.value.email,
+      }));
+    });
+
+    it('should display form field', () => {
+      expect(mailFF).toBeTruthy();
+    });
+
+    it('should display input', () => {
+      expect(input).toBeTruthy();
+    });
+
+    it('should apply input changes', async () => {
+      let newMail = "new@mail.com";
+      await input.setValue(newMail);
+      expect(host.fc.value).toEqual({
+        ...details,
+        email: newMail,
+      });
+    });
   });
 });
